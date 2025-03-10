1aff2dbfbec6833457bd4f34ee931b3e51b47472
govardhansbhati <mailtogovardhansinghbhati@gmail.com>
2025-03-10 21:58:06 +0530

    lightSwitch animation

---
+++ LightSwitch/LightSwitch/LightSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightSwitchApp.swift
+//  LightSwitch
+//
+//  Created by govardhan singh on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LightSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
