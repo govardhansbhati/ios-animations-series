e078cef17eb3fda985f2b54b0e8d5f35af41d684
govardhansbhati <mailtogovardhansinghbhati@gmail.com>
2025-03-12 22:45:21 +0530

    Weather Animation app added Data Model

---
+++ WeatherAnimation/WeatherAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherAnimation
+//
+//  Created by govardhan singh on 12/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherAnimation/WeatherAnimation/WeatherAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAnimationApp.swift
+//  WeatherAnimation
+//
+//  Created by govardhan singh on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
