50aba1979d6366f66bbdc1fb798bad74308ce87d
govardhansbhati <mailtogovardhansinghbhati@gmail.com>
2025-03-23 18:54:06 +0530

    updated code

--- Elevator/Elevator/ContentView.swift
+++ Elevator/Elevator/ContentView.swift
@@ -9,17 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var appData: DataModel
-    
-      
+    let backgroundColor = Color(Color.black)
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            backgroundColor.edgesIgnoringSafeArea(.all)
+            
+            //MARK: Add the elevator and people
+            ElevatorAndPeopleView(doorsOpened: $appData.doorsOpened)
+            
+            // MARK: Button
+            GeometryReader { geo in
+                Button(action: {
+                    appData.doorsOpened.toggle()
+                }) {
+                    /// it the doors are opened make the button white, otherwise make it black
+                    Circle().frame(width: 10, height: 10)
+                        .foregroundStyle(appData.doorsOpened ? Color.white : Color.black)
+                        .overlay {
+                            Circle().stroke(Color.red, lineWidth: 1)
+                        }
+                        .padding(5)
+                        .background(Color.black)
+                        .cornerRadius(30)
+                }
+                .position(x: (geo.size.width / 33), y: (geo.size.height / 2))
+            }
         }
-        .padding()
     }
 }
 
