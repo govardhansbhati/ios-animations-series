117aedf0376a1294c5711e9d1e1b903c5023b351
govardhansbhati <mailtogovardhansinghbhati@gmail.com>
2025-03-31 21:45:59 +0530

    code updated

--- PresentAndDismissTransition/PresentAndDismissTransition/ContentView.swift
+++ PresentAndDismissTransition/PresentAndDismissTransition/ContentView.swift
@@ -19,6 +19,31 @@ struct ContentView: View {
     }
 }
 
+struct PresentAndDismiss: GeometryEffect {
+    var offSetValue: Double
+    var animatableData: Double {
+        get { offSetValue }
+        set { offSetValue = newValue }
+    }
+    
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        let rotationOffset = offSetValue
+        let angleOfRotation = CGFloat(Angle(degrees: 95 * (1 - rotationOffset)).radians)
+        var transform3D = CATransform3DIdentity
+        transform3D.m34 = -1 / max(size.width, size.height)
+        transform3D = CATransform3DRotate(transform3D, angleOfRotation, 1, 0, 0)
+        transform3D = CATransform3DTranslate(transform3D, -size.width/2.0, -size.height/2.0, 0)
+        let transformAffine1 = ProjectionTransform(CGAffineTransform(translationX: size.width/2.0, y: size.height/2.0))
+        let transformAffine2 = ProjectionTransform(CGAffineTransform(scaleX: CGFloat(offSetValue * 2), y: CGFloat(offSetValue * 2)))
+        
+        if offSetValue <= 0.5 {
+            return ProjectionTransform(transform3D).concatenating(transformAffine2).concatenating(transformAffine1)
+        } else {
+            return ProjectionTransform(transform3D).concatenating(transformAffine1)
+        }
+    }
+}
+
 #Preview {
     ContentView()
 }
